f5bd6637e88ec0e8a939ae0cea516ca5ffcb8cd4
Kaushal Elsewhere <kaushal.exe@gmail.com>
2016-05-27 20:26:45 +1000

    Pod setup

---
+++ ImageLocker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageLocker
+//
+//  Created by Kaushal Elsewhere on 27/05/16.
+//  Copyright © 2016 Elsewhere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-ImageLocker/Pods-ImageLocker-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ImageLockerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ImageLockerVersionString[];
+
